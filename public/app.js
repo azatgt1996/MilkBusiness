d972a256db7ab8a8c4d995871c5bff340299dbe7
Azat Galyautdinov <azatgt1996@yandex.ru>
2025-12-29 22:15:51 +0300

    bugfixed

--- public/app.js
+++ public/app.js
@@ -40,13 +40,125 @@ setWidth('#report-table-2', [140, 60, 80])
 
 createApp({
   setup() {
+    const store = ref({ clients: [], deals: [] })
     const currentPage = ref(ls.get('currentPage') ?? 'auth')
-    const currentUser = ref(ls.get('currentUser') ?? '')
     const loading = ref(true)
 
     watch(currentPage, (val) => ls.set('currentPage', currentPage.value))
 
-    const store = ref({ clients: [], deals: [] })
+    onMounted(async () => {
+      store.value = await service.getData()
+      loading.value = false
+    })
+
+    async function saveStore() {
+      loading.value = true
+      await service.saveData(store.value)
+      loading.value = false
+    }
+
+    // AUTH
+    const auth = ref({ login: '', password: '' })
+    const currentUser = ref(ls.get('currentUser') ?? '')
+
+    async function enter() {
+      const { login, password } = auth.value
+      const passwordHash = await fastHash(password)
+      if (login === 'admin') {
+        if (passwordHash !== 'avcrry') return notyf.error('Неверный пароль')
+        currentPage.value = 'deals'
+      } else {
+        const client = store.value.clients.find((it) => it.login === login)
+        if (!client) return notyf.error('Пользователь не найден')
+        if (client.passwordHash !== passwordHash) return notyf.error('Неверный пароль')
+        currentPage.value = 'sales'
+      }
+      currentUser.value = login
+      ls.set('currentUser', currentUser.value)
+    }
+
+    function exit() {
+      currentPage.value = 'auth'
+      ls.set('currentUser', currentUser.value)
+      auth.value = { login: '', password: '' }
+    }
+
+    // CLIENTS
+    const _client = { login: '', password: '', fio: '', address: '', phone: '' }
+    const client = ref($clone(_client))
+
+    async function saveClient() {
+      const { login, password, fio, address, phone } = client.value
+      if (login === 'admin' || store.value.clients.find((it) => it.login === login)) {
+        return notyf.error('Клиент с таким логином уже есть')
+      }
+      if (store.value.clients.find((it) => it.fio === fio)) {
+        return notyf.error('Клиент с таким ФИО уже есть')
+      }
+      const passwordHash = fastHash(password)
+      store.value.clients.push({ login, passwordHash, fio, address, phone })
+      await saveStore()
+      notyf.success('Клиент добавлен')
+      client.value = $clone(_client)
+      currentPage.value = 'clients'
+    }
+
+    // DEALS
+    const _deal = {
+      fio: '',
+      date: today,
+      volume: null,
+      price: null,
+      proteins: undefined,
+      fats: undefined,
+    }
+
+    const deal = ref($clone(_deal))
+    const dealIndex = ref(null)
+    const dateFilter = ref(null)
+
+    const filteredDeals = computed(() => {
+      if (!dateFilter.value) return store.value.deals
+      return store.value.deals.filter((it) => it.date === dateFilter.value)
+    })
+
+    const sales = computed(() => {
+      if (currentUser.value === 'admin') return []
+      const client = store.value.clients.find((it) => it.login === currentUser.value)
+      return store.value.deals.filter((it) => it.fio === client.fio)
+    })
+
+    const sortedClients = computed(() => {
+      return store.value.clients.map((it) => it.fio).toSorted()
+    })
+
+    function openDeal(index = null) {
+      dealIndex.value = index
+      deal.value = $clone(index !== null ? store.value.deals[index] : _deal)
+      currentPage.value = 'dealCard'
+    }
+
+    async function saveDeal() {
+      const { fio, date, volume, price, proteins, fats } = deal.value
+      const data = {
+        fio,
+        date,
+        volume,
+        price,
+        proteins: proteins || undefined,
+        fats: fats || undefined,
+      }
+      if (dealIndex.value !== null) {
+        store.value.deals[dealIndex.value] = data
+      } else {
+        store.value.deals.push(data)
+      }
+      await saveStore()
+      notyf.success(dealIndex.value !== null ? 'Сделка изменена' : 'Сделка добавлена')
+      currentPage.value = 'deals'
+    }
+
+    // REPORT
     const period = ref('all')
 
     const periods = computed(() => {
@@ -83,10 +195,6 @@ createApp({
       }
     })
 
-    const sortedClients = computed(() => {
-      return store.value.clients.map((it) => it.fio).toSorted()
-    })
-
     function gotoReport() {
       currentPage.value = 'report'
       period.value = 'all'
@@ -94,113 +202,6 @@ createApp({
 
     function backFromReport() {
       currentPage.value = currentUser.value === 'admin' ? 'deals' : 'sales'
-    }
-
-    onMounted(async () => {
-      store.value = await service.getData()
-      loading.value = false
-    })
-
-    async function saveStore() {
-      loading.value = true
-      await service.saveData(store.value)
-      loading.value = false
-    }
-
-    const auth = ref({ login: '', password: '' })
-
-    async function enter() {
-      const { login, password } = auth.value
-      const passwordHash = await fastHash(password)
-      if (login === 'admin') {
-        if (passwordHash !== 'avcrry') {
-          return notyf.error('Неверный пароль')
-        }
-        currentPage.value = 'deals'
-      } else {
-        const client = store.value.clients.find((it) => it.login === login)
-        if (!client) return notyf.error('Пользователь не найден')
-        if (client.passwordHash !== passwordHash) return notyf.error('Неверный пароль')
-        currentPage.value = 'sales'
-      }
-      currentUser.value = login
-      ls.set('currentUser', currentUser.value)
-    }
-
-    function exit() {
-      currentPage.value = 'auth'
-      ls.set('currentUser', currentUser.value)
-      auth.value = { login: '', password: '' }
-    }
-
-    const sales = computed(() => {
-      if (currentUser.value === 'admin') return []
-
-      const client = store.value.clients.find((it) => it.login === currentUser.value)
-      return store.value.deals.filter((it) => it.fio === client.fio)
-    })
-
-    const _client = { login: '', password: '', fio: '', address: '', phone: '' }
-    const client = ref($clone(_client))
-
-    async function saveClient() {
-      const { login, password, fio, address, phone } = client.value
-      if (login === 'admin' || store.value.clients.find((it) => it.login === login)) {
-        return notyf.error('Клиент с таким логином уже есть')
-      }
-      if (store.value.clients.find((it) => it.fio === fio)) {
-        return notyf.error('Клиент с таким ФИО уже есть')
-      }
-      const passwordHash = fastHash(password)
-      store.value.clients.push({ login, passwordHash, fio, address, phone })
-      await saveStore()
-      notyf.success('Клиент добавлен')
-      client.value = $clone(_client)
-      currentPage.value = 'clients'
-    }
-
-    const _deal = {
-      fio: '',
-      date: today,
-      volume: null,
-      price: null,
-      proteins: undefined,
-      fats: undefined,
-    }
-
-    const deal = ref($clone(_deal))
-    const dealIndex = ref(null)
-    const dateFilter = ref(null)
-
-    const filteredDeals = computed(() => {
-      if (!dateFilter.value) return store.value.deals
-      return store.value.deals.filter((it) => it.date === dateFilter.value)
-    })
-
-    function openDeal(index = null) {
-      dealIndex.value = index
-      deal.value = $clone(index !== null ? store.value.deals[index] : _deal)
-      currentPage.value = 'dealCard'
-    }
-
-    async function saveDeal() {
-      const { fio, date, volume, price, proteins, fats } = deal.value
-      const data = {
-        fio,
-        date,
-        volume,
-        price,
-        proteins: proteins || undefined,
-        fats: fats || undefined,
-      }
-      if (dealIndex.value !== null) {
-        store.value.deals[dealIndex.value] = data
-      } else {
-        store.value.deals.push(data)
-      }
-      await saveStore()
-      notyf.success(dealIndex.value !== null ? 'Сделка изменена' : 'Сделка добавлена')
-      currentPage.value = 'deals'
     }
 
     return {
